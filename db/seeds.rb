9601b66b546af0155b11f8e3229b405015cd6fb7
codeamac07 <codeamac07@gmail.com>
2017-05-26 14:31:04 -0500

    Quora exercise

---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+User.create (name: "Davo", last_name: "Valdes", email: "davotanko@hotmail.com", password: "oa")
+User.create (name: "Juan", last_name: "Valdes", email: "juan@hotmail.com", password: "test")
+User.create (name: "Alex", last_name: "Déciga", email: "alex@hotmail.com", password: "oa")
+User.create (name: "Juan", last_name: "García", email: "jujujuan@hotmail.com", password: "test")
+
+Question.create ("Este es el primer post de prueba")
+Question.create ("Este es el segundo post de prueba")
+Question.create ("Este es el tercero post de prueba")
+Question.create ("Este es el cuarto post de prueba")
+Question.create ("Este es el quinto post de prueba")
+
+
+Answer.create("esta es la primer respuesta")
+Answer.create("esta es la segunda respuesta")
+Answer.create("esta es la tercera respuesta")
+Answer.create("esta es la cuarta respuesta")
+Answer.create("esta es la quinta respuesta")
+Answer.create("esta es la sexta respuesta")
+Answer.create("esta es la séptima respuesta")
